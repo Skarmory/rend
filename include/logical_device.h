7a86d2f65f2d4d434ab8eb54d2e809b6aef643ea
Alastair Paterson <alas.paterson@gmail.com>
2018-12-10 23:59:14 +0000

    Code tidy up

--- include/logical_device.h
+++ include/logical_device.h
@@ -43,58 +43,58 @@ public:
     LogicalDevice& operator=(LogicalDevice&&) = delete;
 
     // Retrieval
-    const DeviceContext&     get_device_context(void) const;
-    const PhysicalDevice&    get_physical_device(void) const;
-    const QueueFamily*       get_graphics_queue_family(void) const;
-    const QueueFamily*       get_transfer_queue_family(void) const;
-    VkDevice                 get_handle(void) const;
-    VkQueue                  get_queue(QueueType type) const;
+    const DeviceContext&  get_device_context(void) const;
+    const PhysicalDevice& get_physical_device(void) const;
+    const QueueFamily*    get_graphics_queue_family(void) const;
+    const QueueFamily*    get_transfer_queue_family(void) const;
+    VkDevice              get_handle(void) const;
+    VkQueue               get_queue(QueueType type) const;
 
-    bool                     queue_submit(const std::vector<CommandBuffer*>& command_buffers, QueueType type, const std::vector<Semaphore*>& wait_sems, const std::vector<Semaphore*>& signal_sems, Fence* fence);
+    bool                  queue_submit(const std::vector<CommandBuffer*>& command_buffers, QueueType type, const std::vector<Semaphore*>& wait_sems, const std::vector<Semaphore*>& signal_sems, Fence* fence);
 
     // Creational
-    CommandPool*             create_command_pool(const QueueType type, bool can_reset);
-    void                     destroy_command_pool(CommandPool** command_pool);
+    CommandPool*          create_command_pool(const QueueType type, bool can_reset);
+    void                  destroy_command_pool(CommandPool** command_pool);
 
-    Swapchain*               create_swapchain(uint32_t desired_images);
-    void                     destroy_swapchain(Swapchain** swapchain);
+    Swapchain*            create_swapchain(uint32_t desired_images);
+    void                  destroy_swapchain(Swapchain** swapchain);
 
-    RenderPass*              create_render_pass(const std::vector<VkAttachmentDescription>& attachment_descs, const std::vector<VkSubpassDescription>& subpass_descs, const std::vector<VkSubpassDependency>& subpass_deps);
-    void                     destroy_render_pass(RenderPass** render_pass);
+    RenderPass*           create_render_pass(const std::vector<VkAttachmentDescription>& attachment_descs, const std::vector<VkSubpassDescription>& subpass_descs, const std::vector<VkSubpassDependency>& subpass_deps);
+    void                  destroy_render_pass(RenderPass** render_pass);
 
-    Framebuffer*             create_framebuffer(const RenderPass& render_pass, const std::vector<VkImageView>& image_views, VkExtent3D dimensions);
-    void                     destroy_framebuffer(Framebuffer** framebuffer);
+    Framebuffer*          create_framebuffer(const RenderPass& render_pass, const std::vector<VkImageView>& image_views, VkExtent3D dimensions);
+    void                  destroy_framebuffer(Framebuffer** framebuffer);
 
-    DescriptorPool*          create_descriptor_pool(uint32_t max_sets, const std::vector<VkDescriptorPoolSize>& pool_sizes);
-    void                     destroy_descriptor_pool(DescriptorPool** pool);
+    DescriptorPool*       create_descriptor_pool(uint32_t max_sets, const std::vector<VkDescriptorPoolSize>& pool_sizes);
+    void                  destroy_descriptor_pool(DescriptorPool** pool);
 
-    DescriptorSetLayout*     create_descriptor_set_layout(const std::vector<VkDescriptorSetLayoutBinding>& bindings);
-    void                     destroy_descriptor_set_layout(DescriptorSetLayout** layout);
+    DescriptorSetLayout*  create_descriptor_set_layout(const std::vector<VkDescriptorSetLayoutBinding>& bindings);
+    void                  destroy_descriptor_set_layout(DescriptorSetLayout** layout);
 
-    PipelineLayout*          create_pipeline_layout(const std::vector<DescriptorSetLayout*>& desc_set_layouts);
-    void                     destroy_pipeline_layout(PipelineLayout** layout);
+    PipelineLayout*       create_pipeline_layout(const std::vector<DescriptorSetLayout*>& desc_set_layouts);
+    void                  destroy_pipeline_layout(PipelineLayout** layout);
 
-    Pipeline*                create_pipeline(PipelineSettings* settings);
-    void                     destroy_pipeline(Pipeline** pipeline);
+    Pipeline*             create_pipeline(PipelineSettings* settings);
+    void                  destroy_pipeline(Pipeline** pipeline);
 
-    Shader*                  create_shader(const void* data, uint32_t size_bytes, ShaderType type);
-    void                     destroy_shader(Shader** shader);
+    Shader*               create_shader(const void* data, uint32_t size_bytes, ShaderType type);
+    void                  destroy_shader(Shader** shader);
 
-    Semaphore*               create_semaphore(void);
-    void                     destroy_semaphore(Semaphore** semaphore);
+    Semaphore*            create_semaphore(void);
+    void                  destroy_semaphore(Semaphore** semaphore);
 
-    Fence*                   create_fence(bool start_signalled=false);
-    void                     destroy_fence(Fence** fence);
+    Fence*                create_fence(bool start_signalled=false);
+    void                  destroy_fence(Fence** fence);
 
-    Event*                   create_event(void);
-    void                     destroy_event(Event** event);
+    Event*                create_event(void);
+    void                  destroy_event(Event** event);
 
 private:
     VkDevice _vk_device;
-    VkQueue _vk_graphics_queue;
-    VkQueue _vk_transfer_queue;
+    VkQueue  _vk_graphics_queue;
+    VkQueue  _vk_transfer_queue;
 
-    const DeviceContext* _context;
+    const DeviceContext*  _context;
     const PhysicalDevice* _physical_device;
 
     const QueueFamily* const _graphics_family;
