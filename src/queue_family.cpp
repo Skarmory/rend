b5463d9d271a00c88563f5139d16727f08f3d54d
Alastair Paterson <alas.paterson@gmail.com>
2019-01-23 02:15:57 +0000

    Removed debug printing from queue family

--- src/queue_family.cpp
+++ src/queue_family.cpp
@@ -1,17 +1,13 @@
 #include "queue_family.h"
 
-#include <iostream>
-
 using namespace rend;
 
 QueueFamily::QueueFamily(uint32_t index, const VkQueueFamilyProperties& properties, bool supports_present) : _index(index), _properties(properties), _supports_present_queue(supports_present)
 {
-    std::cout << "Constructing queue family" << std::endl;
 }
 
 QueueFamily::~QueueFamily(void)
 {
-    std::cout << "Destructing queue family" << std::endl;
 }
 
 uint32_t QueueFamily::get_index(void) const
