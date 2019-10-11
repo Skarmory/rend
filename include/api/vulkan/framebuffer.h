7b4519651ba2a97a34fe7209d8ba60f52821fd28
Alastair Paterson <alas.paterson@gmail.com>
2019-10-11 21:49:20 +0100

    Ability to add render targets to framebuffer

--- include/api/vulkan/framebuffer.h
+++ include/api/vulkan/framebuffer.h
@@ -7,8 +7,10 @@
 namespace rend
 {
 
+class DepthBuffer;
 class DeviceContext;
 class RenderPass;
+class RenderTarget;
 
 class Framebuffer
 {
@@ -21,12 +23,21 @@ public:
     Framebuffer& operator=(const Framebuffer&) = delete;
     Framebuffer& operator=(Framebuffer&&)      = delete;
 
+    // These two are for internal use only right now
     bool create_framebuffer(const RenderPass& render_pass, const std::vector<VkImageView>& attachments, VkExtent3D dimensions);
     bool recreate(const std::vector<VkImageView>& attachments, VkExtent3D dimensions);
 
-    VkFramebuffer                  get_handle(void) const;
-    const VkFramebufferCreateInfo& get_vk_create_info(void) const;
-    const RenderPass*              get_render_pass(void) const;
+    bool create_framebuffer(const RenderPass& render_pass, VkExtent3D dimensions);
+    bool recreate(VkExtent3D dimensions);
+
+    bool add_render_target(RenderTarget& target);
+    bool set_depth_buffer(DepthBuffer& buffer);
+
+    VkFramebuffer                     get_handle(void) const;
+    const VkFramebufferCreateInfo&    get_vk_create_info(void) const;
+    const RenderPass*                 get_render_pass(void) const;
+    const std::vector<RenderTarget*>& get_render_targets(void) const;
+    const DepthBuffer*                get_depth_buffer(void) const;
 
 private:
     bool _create(const std::vector<VkImageView>& attachments, VkExtent3D dimensions);
@@ -35,6 +46,9 @@ private:
 private:
     DeviceContext&          _context;
     const RenderPass*       _render_pass;
+
+    std::vector<RenderTarget*> _render_targets;
+    DepthBuffer*               _depth_buffer;
 
     VkFramebufferCreateInfo _vk_create_info;
     VkFramebuffer           _vk_framebuffer;
--- src/api/vulkan/framebuffer.cpp
+++ src/api/vulkan/framebuffer.cpp
@@ -1,8 +1,10 @@
 #include "framebuffer.h"
 
+#include "depth_buffer.h"
 #include "device_context.h"
 #include "logical_device.h"
 #include "render_pass.h"
+#include "render_target.h"
 
 using namespace rend;
 
@@ -17,6 +19,26 @@ Framebuffer::Framebuffer(DeviceContext& context)
 Framebuffer::~Framebuffer(void)
 {
     _destroy();
+}
+
+bool Framebuffer::add_render_target(RenderTarget& target)
+{
+    if(_vk_framebuffer != VK_NULL_HANDLE)
+        return false;
+
+    _render_targets.push_back(&target);
+
+    return true;
+}
+
+bool Framebuffer::set_depth_buffer(DepthBuffer& buffer)
+{
+    if(_vk_framebuffer != VK_NULL_HANDLE)
+        return false;
+
+    _depth_buffer = &buffer;
+
+    return true;
 }
 
 bool Framebuffer::create_framebuffer(const RenderPass& render_pass, const std::vector<VkImageView>& attachments, VkExtent3D dimensions)
@@ -37,6 +59,8 @@ bool Framebuffer::create_framebuffer(const RenderPass& render_pass, const std::v
         .layers          = 0
     };
 
+    _render_pass = &render_pass;
+
     return _create(attachments, dimensions);
 }
 
@@ -44,6 +68,52 @@ bool Framebuffer::recreate(const std::vector<VkImageView>& attachments, VkExtent
 {
     if(_vk_framebuffer == VK_NULL_HANDLE)
         return false;
+
+    _destroy();
+    return _create(attachments, dimensions);
+}
+
+bool Framebuffer::create_framebuffer(const RenderPass& render_pass, VkExtent3D dimensions)
+{
+    if(_vk_framebuffer != VK_NULL_HANDLE)
+        return false;
+
+    _vk_create_info =
+    {
+        .sType           = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO,
+        .pNext           = nullptr,
+        .flags           = 0,
+        .renderPass      = render_pass.get_handle(),
+        .attachmentCount = 0,
+        .pAttachments    = nullptr,
+        .width           = 0,
+        .height          = 0,
+        .layers          = 0
+    };
+
+    std::vector<VkImageView> attachments;
+    for(RenderTarget* target : _render_targets)
+        attachments.push_back(target->get_view());
+
+    if(_depth_buffer)
+        attachments.push_back(_depth_buffer->get_view());
+
+    _render_pass = &render_pass;
+
+    return _create(attachments, dimensions);
+}
+
+bool Framebuffer::recreate(VkExtent3D dimensions)
+{
+    if(_vk_framebuffer == VK_NULL_HANDLE)
+        return false;
+
+    std::vector<VkImageView> attachments;
+    for(RenderTarget* target : _render_targets)
+        attachments.push_back(target->get_view());
+
+    if(_depth_buffer)
+        attachments.push_back(_depth_buffer->get_view());
 
     _destroy();
     return _create(attachments, dimensions);
@@ -62,6 +132,16 @@ const VkFramebufferCreateInfo& Framebuffer::get_vk_create_info(void) const
 const RenderPass* Framebuffer::get_render_pass(void) const
 {
     return _render_pass;
+}
+
+const std::vector<RenderTarget*>& Framebuffer::get_render_targets(void) const
+{
+    return _render_targets;
+}
+
+const DepthBuffer* Framebuffer::get_depth_buffer(void) const
+{
+    return _depth_buffer;
 }
 
 bool Framebuffer::_create(const std::vector<VkImageView>& attachments, VkExtent3D dimensions)
