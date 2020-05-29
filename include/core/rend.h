f1635acd58a2f082202ff05412f627a7f9789199
Alastair Paterson <Skarmory@users.noreply.github.com>
2020-05-30 00:08:56 +0100

    Added an init method for rend
    Trying to make initialisation easier, the init method must be called and
    it will initialise everything in the correct order

---
+++ include/core/rend.h
@@ -0,0 +1,11 @@
+#ifndef REND_REND_H
+#define REND_REND_H
+
+namespace rend
+{
+	class Window;
+
+	void init_rend(Window& window);
+}
+
+#endif
---
+++ src/core/rend.cpp
@@ -0,0 +1,37 @@
+#include "rend.h"
+
+#include "device_context.h"
+#include "vulkan_instance.h"
+#include "window.h"
+#include "window_context.h"
+
+#include <vector>
+
+void rend::init_rend(Window& window)
+{
+	std::vector<const char*> extensions =
+	{
+		"VK_KHR_surface"
+#ifdef __linux__
+		, "VK_KHR_xcb_surface"
+#elif defined(_WIN32)
+		, "VK_KHR_win32_surface"
+#endif
+	};
+
+	std::vector<const char*> layers =
+	{
+		"VK_LAYER_LUNARG_standard_validation"
+	};
+
+	// Init singletons
+	auto& vk_instance = rend::vkal::VulkanInstance::instance();
+	vk_instance.create_instance(extensions.data(), extensions.size(), layers.data(), layers.size());
+
+	auto& window_context = WindowContext::instance();
+	window.create_window();
+	window_context.set_window(window);
+
+	auto& context = DeviceContext::instance();
+	context.create();
+}
