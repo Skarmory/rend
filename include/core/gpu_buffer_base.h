850f5b9350ead8a4d781f9352eb80da1bf93e131
Alastair Paterson <Skarmory@users.noreply.github.com>
2020-06-20 13:00:20 +0100

    Refactor GPUBufferBase to hold handles

--- include/core/gpu_buffer_base.h
+++ include/core/gpu_buffer_base.h
@@ -2,6 +2,7 @@
 #define REND_GPU_BUFFER_BASE_H
 
 #include "gpu_resource.h"
+#include "rend_defs.h"
 
 namespace rend
 {
@@ -9,13 +10,18 @@ namespace rend
 class GPUBufferBase : public GPUResource
 {
 public:
-    explicit GPUBufferBase(void);
-    ~GPUBufferBase(void);
+    GPUBufferBase(void) = default;
+    ~GPUBufferBase(void) = default;
 
     GPUBufferBase(const GPUBufferBase&)            = delete;
     GPUBufferBase(GPUBufferBase&&)                 = delete;
     GPUBufferBase& operator=(const GPUBufferBase&) = delete;
     GPUBufferBase& operator=(GPUBufferBase&&)      = delete;
+
+    BufferHandle get_handle(void) const { return _handle; }
+
+protected:
+    BufferHandle _handle{ NULL_HANDLE };
 };
 
 }
