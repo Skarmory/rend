dd804a4b9e03795fca8f794ae3365f4768d628bb
Alastair Paterson <Skarmory@users.noreply.github.com>
2020-06-20 13:14:21 +0100

    Fix whitespace problems

--- src/core/index_buffer.cpp
+++ src/core/index_buffer.cpp
@@ -6,14 +6,14 @@ using namespace rend;
 
 bool IndexBuffer::create_index_buffer(uint32_t indices_count, size_t index_size)
 {
-	auto& ctx = DeviceContext::instance();
+    auto& ctx = DeviceContext::instance();
 
-	_handle = ctx.create_index_buffer(indices_count, index_size);
+    _handle = ctx.create_index_buffer(indices_count, index_size);
 
-	if (_handle != NULL_HANDLE)
-	{
-		_bytes = indices_count * index_size;
-	}
+    if (_handle != NULL_HANDLE)
+    {
+        _bytes = indices_count * index_size;
+    }
 
-	return _handle;
+    return _handle;
 }
