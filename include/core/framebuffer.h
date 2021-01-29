1be5047fe5baac31b4fde235e0150c44db18268b
Song Taezhou <song.taezhou@gmail.com>
2021-01-29 15:35:54 +0000

    Deleted api-specific Framebuffer
    Added generic rend Framebuffer

---
+++ include/core/framebuffer.h
@@ -0,0 +1,40 @@
+#ifndef REND_FRAME_BUFFER_H
+#define REND_FRAME_BUFFER_H
+
+#include "rend_constants.h"
+#include "rend_defs.h"
+
+namespace rend
+{
+
+class GPUTexture;
+class RenderPass;
+
+class Framebuffer
+{
+public:
+    Framebuffer(void)                          = default;
+    ~Framebuffer(void)                         = default;
+    Framebuffer(const Framebuffer&)            = delete;
+    Framebuffer(Framebuffer&&)                 = delete;
+    Framebuffer& operator=(const Framebuffer&) = delete;
+    Framebuffer& operator=(Framebuffer&&)      = delete;
+
+    StatusCode create(const FramebufferInfo& info);
+    void       destroy(void);
+
+    FramebufferHandle handle(void) const;
+    //RenderPassHandle  render_pass(void) const;
+    //Texture2DHandle   depth_buffer(void) const;
+
+    void on_end_render_pass(void);
+
+private:
+    FramebufferHandle   _handle{ NULL_HANDLE };
+    FramebufferInfo     _framebuffer_info{};
+};
+
+}
+
+#endif
+
---
+++ src/core/framebuffer.cpp
@@ -0,0 +1,69 @@
+#include "framebuffer.h"
+
+#include "device_context.h"
+#include "gpu_texture.h"
+#include "logical_device.h"
+#include "render_pass.h"
+#include "vulkan_device_context.h"
+
+#include <cassert>
+
+using namespace rend;
+
+StatusCode Framebuffer::create(const FramebufferInfo& info)
+{
+    assert(_handle == NULL_HANDLE && "Attempt to create a Framebuffer that has already been created.");
+
+    auto& ctx = static_cast<VulkanDeviceContext&>(DeviceContext::instance());
+    _handle = ctx.create_framebuffer(info);
+    if(_handle == NULL_HANDLE)
+    {
+        return StatusCode::FAILURE;
+    }
+
+    _framebuffer_info = info;
+
+    return StatusCode::SUCCESS;
+}
+
+void Framebuffer::destroy(void)
+{
+    auto& ctx = static_cast<VulkanDeviceContext&>(DeviceContext::instance());
+    ctx.destroy_framebuffer(_handle);
+
+    _handle = NULL_HANDLE;
+    _framebuffer_info = {};
+}
+
+FramebufferHandle Framebuffer::handle(void) const
+{
+    return _handle;
+}
+
+//const RenderPass* Framebuffer::render_pass(void) const
+//{
+//    return _render_pass;
+//}
+//
+//Texture2DHandle Framebuffer::depth_buffer(void) const
+//{
+//    return _depth_buffer;
+//}
+
+void Framebuffer::on_end_render_pass(void)
+{
+    //const std::vector<VkAttachmentDescription>& descs = _render_pass->get_attachment_descs();
+    //for(uint32_t colour_attach_idx = 0; colour_attach_idx < _render_targets.size(); ++colour_attach_idx)
+    //{
+    //    _render_targets[colour_attach_idx]->transition(
+    //        vulkan_helpers::convert_image_layout(descs[colour_attach_idx].finalLayout)
+    //    );
+    //}
+
+    //if(_depth_buffer)
+    //{
+    //    _depth_buffer->transition(
+    //        vulkan_helpers::convert_image_layout(descs.back().finalLayout)
+    //    );
+    //}
+}
