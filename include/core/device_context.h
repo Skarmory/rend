91d51a6b98e54265081fd84ac0b5ee787aa7865f
Song Taezhou <song.taezhou@gmail.com>
2021-06-01 20:38:05 +0100

    Added more functions to device_context

--- include/core/device_context.h
+++ include/core/device_context.h
@@ -11,6 +11,8 @@
 namespace rend
 {
 
+class PipelineInfo;
+
 class DeviceContext
 {
 public:
@@ -26,12 +28,23 @@ public:
     [[nodiscard]] virtual ShaderHandle        create_shader(const ShaderType type, const void* code, const size_t bytes) = 0;
     [[nodiscard]] virtual FramebufferHandle   create_framebuffer(const FramebufferInfo& info) = 0;
     [[nodiscard]] virtual RenderPassHandle    create_render_pass(const RenderPassInfo& info) = 0;
+    [[nodiscard]] virtual PipelineHandle      create_pipeline(const PipelineInfo& info) = 0;
+    [[nodiscard]] virtual CommandPoolHandle   create_command_pool(void) = 0;
+    [[nodiscard]] virtual CommandBufferHandle create_command_buffer(CommandPoolHandle pool_handle) = 0;
 
     virtual void destroy_buffer(BufferHandle handle) = 0;
     virtual void destroy_texture(Texture2DHandle handle ) = 0;
     virtual void destroy_shader(ShaderHandle handle) = 0;
     virtual void destroy_framebuffer(FramebufferHandle handle) = 0;
     virtual void destroy_render_pass(RenderPassHandle handle) = 0;
+    virtual void destroy_pipeline(PipelineHandle handle) = 0;
+    virtual void destroy_command_buffer(CommandBufferHandle handle) = 0;
+
+    virtual void bind_pipeline(CommandBufferHandle cmd_buffer, PipelineBindPoint bind_point, PipelineHandle handle) = 0;
+    virtual void command_buffer_reset(CommandBufferHandle command_buffer_handle) = 0;
+    virtual void copy_buffer_to_buffer(CommandBufferHandle command_buffer_handle, BufferHandle src_buffer_handle, BufferHandle dst_buffer_handle, const BufferBufferCopyInfo& info) = 0;
+    virtual void copy_buffer_to_image(CommandBufferHandle command_buffer_handle, BufferHandle src_buffer_handle, TextureHandle dst_texture_handle, const BufferImageCopyInfo& info) = 0;
+    virtual void pipeline_barrier(const CommandBufferHandle command_buffer_handle, const PipelineBarrierInfo& info) = 0;
 
 protected:
     static DeviceContext* _service;
