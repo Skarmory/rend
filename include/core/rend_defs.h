38c30dfd3fc34a527641c519e39691a43886e3c8
Song Taezhou <song.taezhou@gmail.com>
2021-10-31 12:54:26 +0000

    Added DescriptorType enum

--- include/core/rend_defs.h
+++ include/core/rend_defs.h
@@ -209,6 +209,21 @@ enum class CompareOp
     ALWAYS
 };
 
+enum class DescriptorType
+{
+    SAMPLER,
+    COMBINED_IMAGE_SAMPLER,
+    SAMPLED_IMAGE,
+    STORAGE_IMAGE,
+    UNIFORM_TEXEL_BUFFER,
+    STORAGE_TEXEL_BUFFER,
+    UNIFORM_BUFFER,
+    STORAGE_BUFFER,
+    UNIFORM_BUFFER_DYNAMIC,
+    STORAGE_BUFFER_DYNAMIC,
+    INPUT_ATTACHMENT
+};
+
 enum class StencilOp
 {
     KEEP,
