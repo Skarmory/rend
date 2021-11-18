32c98ce2790db5dd9ef92b2bad9e822923883e27
Song Taezhou <song.taezhou@gmail.com>
2021-11-18 12:47:20 +0000

    Added max scissors and max viewports constants

--- include/core/rend_constants.h
+++ include/core/rend_constants.h
@@ -9,11 +9,13 @@ namespace rend
 
 namespace constants
 {
+    constexpr size_t max_blend_constants{ 4 };
+    constexpr size_t max_dynamic_states{ 8 };
     constexpr size_t max_framebuffer_attachments{ 8 };
     constexpr size_t max_subpasses{ 8 };
     constexpr size_t max_vertex_attributes{ 8 };
-    constexpr size_t max_blend_constants{ 4 };
-    constexpr size_t max_dynamic_states{ 9 };
+    constexpr size_t max_viewports{ 8 };
+    constexpr size_t max_scissors{ 8 };
 }
 
 }
