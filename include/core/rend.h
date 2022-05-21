29d8617896cb8754049fa752a5a84f321bb3e9c6
Song Taezhou <song.taezhou@gmail.com>
2022-05-21 12:57:47 +0100

    Revamp init code following service locator update

--- include/core/rend.h
+++ include/core/rend.h
@@ -2,33 +2,37 @@
 #define REND_REND_H
 
 #include <cstdint>
+#include <vulkan.h>
 
 namespace rend
 {
-	class Window;
 
-    enum class API
-    {
-        API_VULKAN
-    };
+class Window;
 
-    struct VulkanInfo
-    {
-        const char** extensions{ nullptr };
-        uint32_t     extensions_count{ 0 };
-        const char** layers{ nullptr };
-        uint32_t     layers_count{ 0 };
-    };
+enum class API
+{
+    API_VULKAN
+};
 
-    struct RendInfo
-    {
-        API     api{ API::API_VULKAN };
-        void*   api_info{ nullptr };
-        Window* window;
-    };
+struct VulkanInfo
+{
+    const char** extensions{ nullptr };
+    uint32_t     extensions_count{ 0 };
+    const char** layers{ nullptr };
+    uint32_t     layers_count{ 0 };
+    VkPhysicalDeviceFeatures features{};
+    VkQueueFlags queues{};
+};
 
-	void init_rend(const RendInfo& info);
-	void destroy_rend(void);
+struct RendInfo
+{
+    API   api{ API::API_VULKAN };
+    void* api_info{ nullptr };
+};
+
+void init_rend(const RendInfo& info);
+void destroy_rend(void);
+
 }
 
 #endif
--- src/core/rend.cpp
+++ src/core/rend.cpp
@@ -1,61 +1,37 @@
 #include "core/rend.h"
 
-#include "api/vulkan/vulkan_instance.h"
-
 #include "core/device_context.h"
+#include "core/rend_service.h"
+#include "api/vulkan/vulkan_instance.h"
 #include "api/vulkan/vulkan_device_context.h"
-#include "core/window.h"
-#include "core/window_context.h"
 
 #include <iostream>
 #include <vector>
 
-namespace
-{
-    void init_vulkan(const rend::VulkanInfo& info)
-    {
-        auto& vk_instance = rend::VulkanInstance::instance();
-        if(vk_instance.create(info.extensions, info.extensions_count, info.layers, info.layers_count) == rend::StatusCode::FAILURE)
-        {
-            std::cerr << "Failed to create vulkan instance!" << std::endl;
-            std::abort();
-        }
-    }
-
-    void init_window(const rend::RendInfo& info)
-    {
-        auto& window_context = rend::WindowContext::instance();
-        if(!info.window->create_window())
-        {
-            std::cerr << "Failed to create window!" << std::endl;
-            std::abort();
-        }
-
-        window_context.set_window(info.window);
-    }
-
-    rend::DeviceContext* ctx{ nullptr };
-}
-
 void rend::init_rend(const RendInfo& info)
 {
+    DeviceContext* context{ nullptr };
+
     switch(info.api)
     {
         case API::API_VULKAN:
         {
-            ::init_vulkan(*static_cast<const VulkanInfo*>(info.api_info));
-            ::init_window(info);
-            ::ctx = new rend::VulkanDeviceContext;
+            const VulkanInfo* vulkan_info = static_cast<const VulkanInfo*>(info.api_info);
+
+            VulkanInstance* vulkan_instance = new VulkanInstance(vulkan_info->extensions, vulkan_info->extensions_count, vulkan_info->layers, vulkan_info->layers_count);
+            vulkan_instance->create_surface(*RendService::window());
+            RendService::provide(vulkan_instance);
+
+            context = new VulkanDeviceContext(vulkan_info->features, vulkan_info->queues);
+
         }
     }
+
+    RendService::provide(context);
 }
 
 void rend::destroy_rend(void)
 {
-    delete ctx;
-
-    WindowContext::instance().window()->destroy_window();
-	WindowContext::instance().set_window(nullptr);
-
-	VulkanInstance::instance().destroy();
+    delete RendService::device_context();
+    delete RendService::vulkan_instance();
 }
