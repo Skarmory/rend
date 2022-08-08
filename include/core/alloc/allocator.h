803c42da7a92cffe430146162206e077d49b1cbe
Song Taezhou <song.taezhou@gmail.com>
2022-08-08 15:08:15 +0100

    Added  alloc/allocator.h

---
+++ include/core/alloc/allocator.h
@@ -0,0 +1,42 @@
+#ifndef REND_CORE_ALLOC_ALLOCATOR_H
+#define REND_CORE_ALLOC_ALLOCATOR_H
+
+#include <utility>
+
+#include "core/containers/data_array_base.h"
+
+namespace rend
+{
+
+template<class T>
+class Allocator
+{
+    public:
+
+        template<typename... Args>
+        [[nodiscard]]
+        T* allocate(Args&&... args)
+        {
+            return new T(std::forward<Args>(args)...);
+        }
+
+        void deallocate(T* obj)
+        {
+            delete obj;
+        }
+
+        template<typename... Args>
+        void construct(T* p, Args&&... args)
+        {
+            new (p) T(std::forward<Args>(args)...);
+        }
+
+        void destruct(T* p)
+        {
+            p->~T();
+        }
+};
+
+}
+
+#endif
