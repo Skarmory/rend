f84d870267fc1735e89bbd450164bf0b6c421315
Song Taezhou <song.taezhou@gmail.com>
2022-09-05 16:52:37 +0100

    Updated RenderPass

--- include/core/render_pass.h
+++ include/core/render_pass.h
@@ -1,15 +1,24 @@
 #ifndef REND_RENDERPASS_H
 #define REND_RENDERPASS_H
 
+#include "core/gpu_resource.h"
 #include "core/rend_defs.h"
+#include "core/sub_pass.h"
+
+#include <string>
+#include <vector>
 
 namespace rend
 {
 
-class RenderPass
+class CommandBuffer;
+struct DrawItem;
+struct PerPassData;
+
+class RenderPass : public GPUResource
 {
 public:
-    explicit RenderPass(const RenderPassInfo& info);
+    explicit RenderPass(const std::string& name, const RenderPassInfo& info);
     ~RenderPass(void);
     RenderPass(const RenderPass&)            = delete;
     RenderPass(RenderPass&&)                 = delete;
@@ -17,10 +26,19 @@ public:
     RenderPass& operator=(RenderPass&&)      = delete;
 
     RenderPassHandle handle(void) const;
+    std::vector<SubPassHandle> get_subpasses(void) const;
+    bool has_next_subpass(void) const;
+
+    void begin(CommandBuffer& command_buffer, PerPassData& per_pass);
+    void next_subpass(CommandBuffer& command_buffer);
+    void end(CommandBuffer& command_buffer);
+
 
 private:
-    RenderPassHandle _handle{ NULL_HANDLE };
-    RenderPassInfo   _info{};
+    RenderPassHandle           _handle{ NULL_HANDLE };
+    RenderPassInfo             _info{};
+    std::vector<SubPassHandle> _subpasses;
+    uint32_t                   _current_subpass{ 0 };
 };
 
 }
--- src/core/render_pass.cpp
+++ src/core/render_pass.cpp
@@ -1,17 +1,35 @@
 #include "core/render_pass.h"
 
+#include "core/command_buffer.h"
 #include "core/device_context.h"
+#include "core/draw_item.h"
+#include "core/framebuffer.h"
+#include "core/mesh.h"
 #include "core/rend_service.h"
 
+#include "api/vulkan/renderer.h"
+
 #include <cassert>
+#include <functional>
 
 using namespace rend;
 
-RenderPass::RenderPass(const RenderPassInfo& info)
-    : _info(info)
+RenderPass::RenderPass(const std::string& name, const RenderPassInfo& info)
+    :   GPUResource(name),
+        _info(info)
 {
     auto& ctx = *RendService::device_context();
-    _handle = ctx.create_render_pass(info);
+    _handle = ctx.create_render_pass(_info);
+
+    auto* rr = RendService::renderer();
+
+    for(int i = 0; i < _info.subpasses_count; ++i)
+    {
+        std::string subpass_name = name + ", Subpass: " + std::to_string(i);
+        _subpasses.emplace_back(
+            rr->create_sub_pass(subpass_name, _info.subpasses[i].shader_set_handle, _handle, i)
+        );
+    }
 }
 
 RenderPass::~RenderPass(void)
@@ -23,4 +41,43 @@ RenderPass::~RenderPass(void)
 RenderPassHandle RenderPass::handle(void) const
 {
     return _handle;
+}
+
+std::vector<SubPassHandle> RenderPass::get_subpasses(void) const
+{
+    return _subpasses;
+}
+
+bool RenderPass::has_next_subpass(void) const
+{
+    return _current_subpass + 1 != _subpasses.size();
+}
+
+void RenderPass::begin(CommandBuffer& command_buffer, PerPassData& per_pass)
+{
+    auto* rr = RendService::renderer();
+    Framebuffer* fb = rr->get_framebuffer(per_pass.framebuffer);
+
+    command_buffer.begin_render_pass(*this, *fb, per_pass.render_area, per_pass.colour_clear, per_pass.depth_clear);
+
+    SubPass* sp = rr->get_sub_pass(_subpasses[_current_subpass]);
+    sp->begin(command_buffer);
+}
+
+void RenderPass::next_subpass(CommandBuffer& command_buffer)
+{
+    if(has_next_subpass())
+    {
+        auto* rr = RendService::renderer();
+        _current_subpass++;
+        command_buffer.next_subpass();
+        SubPass* sp = rr->get_sub_pass(_subpasses[_current_subpass]);
+        sp->begin(command_buffer);
+    }
+}
+
+void RenderPass::end(CommandBuffer& command_buffer)
+{
+    command_buffer.end_render_pass();
+    _current_subpass = 0;
 }
